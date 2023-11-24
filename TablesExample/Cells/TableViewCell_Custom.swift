1a77fe6ff58855b41686770ab6038b891cf9b538
ronaldoavalos <ronaldo@vecinio.mx>
2023-11-24 11:36:55 -0600

    Initial Commit

---
+++ TablesExample/Cells/TableViewCell_Custom.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell_Custom.swift
+//  TablesExample
+//
+//  Created by ronaldo avalos on 24/11/23.
+//
+
+import UIKit
+
+class TableViewCell_Custom: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TablesExample/ViewController.swift
+++ TablesExample/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+// MARK; Referencia a la tabla
+@IBOutlet weak var tableView: UITableView!
+    
+      
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mi-celda")
         // Do any additional setup after loading the view.
+        // Para interactuar implmentar el delegate
+        tableView.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("La celda \(indexPath.row) fue selccionada")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mi-celda",for: indexPath)
+        cell.textLabel?.text = "Soy la celda No \(indexPath.row)"
+        return cell
+    }
+    
 }
+
 
